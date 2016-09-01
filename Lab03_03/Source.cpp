777e059edebea838dd1ed92f84e70108047cd0bf
Phil Fernandez <norcal618@live.com>
2016-09-01 15:30:55 -0700

    final

--- Lab03_03/Source.cpp
+++ Lab03_03/Source.cpp
@@ -5,10 +5,33 @@ Lab03 Question 03
 */
 
 #include <iostream>
-#include <cmath>
+#include <iomanip>
+using namespace std;
 
 int main()
 {
+	// Define and initialize constant PI
+	const double PI = 3.14159265359;
+	// Define variables for radius, height, volume
+	double radius,
+		height,
+		volume;
+
+	// Prompt user for radius and height
+	cout << "Enter the radius of a cylinder: ";
+	cin >> radius;
+	cout << "Enter the height of a cylinder: ";
+	cin >> height;
+
+	// Calculate the volume
+	volume = PI * radius * radius * height;
+
+	// Display radius, height, volume to std::out
+	// Use setprecision(2) and fixed to round to two decimals
+	cout << setprecision(2) << fixed;
+	cout << "The radius is " << radius << "\n";
+	cout << "The height is " << height << "\n";
+	cout << "The volume is " << volume << "\n";
 
 	return 0;
 }
